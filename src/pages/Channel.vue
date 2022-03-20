5da059a0016734b6bfc40972d10daf15edf929b9
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2022-03-20 18:30:38 +0100

    Update Channel.vue

--- src/pages/Channel.vue
+++ src/pages/Channel.vue
@@ -11,7 +11,7 @@
     </header>
 
     <div class="row">
-      <div class="col-4">
+      <div class="col-xs-4 col-lg-2">
         <q-card flat bordered square class="window-height">
           <q-card-section class="server_name_section">
             <div class="row">
@@ -23,11 +23,16 @@
                 text-color="white"
                 icon="groups"
               />
-              <div class="col server_name">FIIT</div>
+              <div class="col server_name">
+                <b>FIIT</b>
+              </div>
             </div>
           </q-card-section>
           <q-separator size="1px" color="black" />
-          <q-card-section class="flex justify-start items-center">
+          <q-card-section
+            style="padding-bottom: 0"
+            class="flex justify-start items-center channel_list"
+          >
             <q-avatar
               icon="expand_more"
               font-size="22px"
@@ -37,23 +42,23 @@
             />
             <h6 class="q-ma-sm">Channels</h6>
           </q-card-section>
-          <q-card-section>
+          <q-card-section style="padding-top: 5px">
             <ul
               class="q-gutter-md"
               style="list-style-type: none; padding-left: 1rem"
             >
-              <li><q-btn flat> # Channel 1</q-btn></li>
+              <li><q-btn flat># Channel 1</q-btn></li>
               <li># Channel 2</li>
               <li># Channel 3</li>
               <li># Channel 4</li>
-              <li>2</li>
-              <li>2</li>
+              <li># Channel 5</li>
+              <li># Channel 6</li>
             </ul>
           </q-card-section>
         </q-card>
       </div>
 
-      <div class="col-8">
+      <div class="col-xs-8 col-lg-10">
         <q-card flat square bordered class="window-height">
           <q-card-section class="channel_header">
             <!-- <q-avatar
@@ -64,7 +69,7 @@
               text-color="white"
               icon=""
             /># Channel 1 -->
-            <div class="">#VPWA</div>
+            <h5><b>#VPWA</b></h5>
           </q-card-section>
           <q-separator size="1px" color="black" />
           <q-card-section> </q-card-section>
@@ -76,6 +81,7 @@
 
 <style scoped lang="scss">
 .server_name {
+  font-size: 25px;
   align-self: center;
 }
 
@@ -89,5 +95,8 @@
   height: 4.5rem;
   display: flex;
   align-items: center;
+}
+
+.channel_list {
 }
 </style>
