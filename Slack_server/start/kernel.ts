33b399b4e6d7dd8726390a4f12a6f7cacc28b6d2
Zajo <ckkoval33@gmail.com>
2022-04-02 15:32:56 +0200

    server

--- Slack_server/start/kernel.ts
+++ Slack_server/start/kernel.ts
@@ -38,7 +38,4 @@ Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser')])
 | Route.get('dashboard', 'UserController.dashboard').middleware('auth')
 |
 */
-
-Server.middleware.registerNamed({
-  auth: () => import('App/Middleware/Auth'),
-})
+Server.middleware.registerNamed({})
