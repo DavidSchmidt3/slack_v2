757d6b67297049954c043dadeeb0fa374e0f70ea
Zajo <ckkoval33@gmail.com>
2022-04-02 16:11:58 +0200

    server configured

---
+++ slack_v2_server/app/Models/Channel.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Message from 'App/Models/Message'
+
+export default class Channel extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Message, {
+    foreignKey: 'channelId',
+  })
+  public messages: HasMany<typeof Message>
+}
